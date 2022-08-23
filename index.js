74831218bd2620dd76c97248f20cdd117284b9ab
Turner Milan <turnermilan@gmail.com>
2022-08-22 21:19:39 -0400

    started work on painting cells with mouseover

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const container = document.getElementById("grid-container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+const rows = document.getElementsByClassName("gridRow");
+const cells = document.getElementsByClassName("cell");
 
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
 
-// Takes (rows, columns) input and makes a grid
 function makeRows(rowNum) {
-
-    // Creates rows
-    for (r = 0; r < rowNum; r++) {
+    for (i = 0; i < rowNum; i++) {
         let row = document.createElement("div");
         container.appendChild(row).className = "gridRow";
     };
 };
 
-// Creates columns
 function makeColumns(cellNum) {
     for (i = 0; i < rows.length; i++) {
         for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "cell";
         };
-
     };
 };
-
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
 defaultGrid()
+
+const paintCell = () =>{
+    cells.className = 'paintedCell'
+}
+
+// This is not working
+// for some reason all of the cells are being selected at once or something
+for(let i = 0; i < cells.length; i++){
+    cells[i].addEventListener('mouseover', paintCell)
+}
