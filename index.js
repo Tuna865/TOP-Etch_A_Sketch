39f65ee044fb24d83c3820dab166e7e7499d0e62
Turner Milan <turnermilan@gmail.com>
2022-08-26 23:38:08 -0400

    completed new grid functionality with mouseover

--- index.js
+++ index.js
@@ -1,58 +1,45 @@
 const container = document.getElementById("grid-container");
 const addButton = document.getElementById("addButton")
 const resetButton = document.getElementById("resetButton")
-const rows = document.getElementsByClassName("gridRow");
-const cells = document.getElementsByClassName("cell");
-
-function makeRows(rowNum) {
-    for (i = 0; i < rowNum; i++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-    };
-};
-function makeGrid(rows, columns) {
-    makeRows(rows);
-    makeColumns(columns);
-}
-makeGrid(16,16)
-
-
-
-let paintedCell = 'paintedCell'
-
-function colorize(event){
-    const target = event.target
-    target.className = paintedCell
+const gridContainer = document.querySelector("#grid-container");
+               
+function createGrid(number) {
+    gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+    for (let i = 1; i <= number * number; i++) {
+        let div = document.createElement("div");
+        div.classList.add("grid-box")
+        gridContainer.appendChild(div);
+    }
+    document.querySelector(".grid-dimensions").textContent = `Current Grid Size: ${number}x${number}`;
 }
 
-for(let i = 0; i < cells.length; i++){
-    cells[i].addEventListener('mouseover', colorize)
+function changeColor(event){
+    event.target.style.backgroundColor = "black";
 }
 
 addButton.addEventListener('click', () => {
-    console.log('add squares button pressed')
     // prompt returns a string, so this needs to be number-ized
     let newGrid = Number(prompt(
-        "How many squares would you like to have on each side?", 
+        "What grid dimensions would you like?", 
         'Enter a number from 1-100'))
-
-    if(newGrid > 100){
-     alert("Please enter a number less than or equal to 100")
-    } else{makeGrid(newGrid, newGrid)}
-
-
+        if(newGrid > 100){
+            alert("Please enter a number less than or equal to 100")
+        } else{
+            while(gridContainer.hasChildNodes()){
+                gridContainer.removeChild(gridContainer.firstChild)
+            }    
+            createGrid(newGrid);
+            let gridBoxList = document.querySelectorAll(".grid-box");
+            gridBoxList.forEach(gridBox => {gridBox.addEventListener("mouseover", changeColor)});
+        }
 })
+
+let defaultGrid = 16;
+createGrid(defaultGrid);
+let gridBoxList = document.querySelectorAll(".grid-box");
+gridBoxList.forEach(gridBox => {gridBox.addEventListener("mouseover", changeColor)});
+
 resetButton.addEventListener("click", () => {
-    setTimeout(startOver = () => {
         window.location.reload()
-    })
 })
